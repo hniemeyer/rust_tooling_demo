4b179491ba814dcd025bc7d53d44fa5113258082
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2020-09-07 11:39:39 +0200

    Function to change array

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use ndarray::prelude::*;
 
+fn change_elem(x: &mut Array2<f64>) {
+
+    x[[0,0]] += 1.0;
+
+}
+
 fn main() {
-    let a = array![
+    let mut a = array![
                 [1.,2.,3.], 
                 [4.,5.,6.],
             ]; 
+    change_elem(&mut a);
     println!("{:?}", a);
 }
