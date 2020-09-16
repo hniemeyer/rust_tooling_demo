3a69f9baea31cff3148d3409e40f8d41bbc0b4b0
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2020-09-16 15:15:54 +0200

    move the rectangle

--- src/geometry.rs
+++ src/geometry.rs
@@ -43,6 +43,10 @@ impl Rectangle {
     pub fn circumference(&self) -> f64 {
         2.0 * self.length + 2.0 * self.width
     }
+
+    pub fn move_direction(&mut self, dx: f64, dy:f64) {
+        self.center.move_direction(dx, dy);
+    }
 }
 
 #[cfg(test)]
@@ -59,5 +63,12 @@ mod tests {
    fn circumference() {
         let my_rect = Rectangle::new(2.0, 3.0, 0.0, 0.0);
         assert_eq!(my_rect.circumference(), 10.0)
+    }
+
+    #[test]
+    fn move_point() {
+        let mut my_point = Point::new(1.0,1.0);
+        my_point.move_direction(-1.0, -1.0);
+        assert_eq!(my_point.x, 0.0);
     }
 }
