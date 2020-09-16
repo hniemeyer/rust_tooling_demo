0d3e5d3f4dc8eb02d153022c20f48bcb2961a1ee
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2020-09-16 15:12:16 +0200

    Center for the Rectangle

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,17 +1,38 @@
 use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: f64,
+    y: f64
+}
+
+impl Point {
+    fn new(xx: f64, yy:f64) -> Self {
+        Point {x: xx, y: yy}
+    }
+
+    fn move_direction(&mut self, dx: f64, dy:f64) {
+        self.x += dx;
+        self.y += dy;
+    }
+}
+
 /// A geomtric rectangle
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Rectangle {
     /// length of the rectangle
     length: f64,
     /// width of the rectangle
-    width: f64
+    width: f64,
+    /// center of the rectangle
+    center: Point
 }
 
 impl Rectangle {
     /// create a new rectangle.
-    pub fn new(l: f64, w: f64) -> Self {
-        Rectangle {length: l, width: w}
+    pub fn new(l: f64, w: f64, x: f64, y: f64) -> Self {
+        let p = Point::new(x, y);
+        Rectangle {length: l, width: w, center: p}
     }
 
     /// calculate the area of the rectangle
@@ -30,13 +51,13 @@ mod tests {
 
     #[test]
     fn area() {
-        let my_rect = Rectangle::new(2.0, 2.0);
+        let my_rect = Rectangle::new(2.0, 2.0, 0.0, 0.0);
         assert_eq!(my_rect.area(), 4.0);
     }
 
     #[test]
    fn circumference() {
-        let my_rect = Rectangle::new(2.0, 3.0);
+        let my_rect = Rectangle::new(2.0, 3.0, 0.0, 0.0);
         assert_eq!(my_rect.circumference(), 10.0)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod geometry;
 
 fn main() {
-    let rect = geometry::Rectangle::new(3.2, 4.5);
+    let rect = geometry::Rectangle::new(3.2, 4.5, 21.2, 33.2);
     println!("{}", rect.area());
     println!("{}", rect.circumference());
     let serialized = serde_json::to_string(&rect).unwrap();
