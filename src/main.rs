0c3fa8206e43ef6a08889e68fc91efa9b6062b62
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2020-09-16 14:42:34 +0200

    Print out circumference

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod geometry;
 fn main() {
     let rect = geometry::Rectangle::new(3.2, 4.5);
     println!("{}", rect.area());
+    println!("{}", rect.circumference());
     let serialized = serde_json::to_string(&rect).unwrap();
     println!("serialized = {}", serialized);
 
