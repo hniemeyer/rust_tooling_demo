edbc6180f47580ef2bc5ad21189685740ef341fc
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2020-09-16 15:19:25 +0200

    Formatted geometry module

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,17 +1,17 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Point {
     x: f64,
-    y: f64
+    y: f64,
 }
 
 impl Point {
-    fn new(xx: f64, yy:f64) -> Self {
-        Point {x: xx, y: yy}
+    fn new(xx: f64, yy: f64) -> Self {
+        Point { x: xx, y: yy }
     }
 
-    fn move_direction(&mut self, dx: f64, dy:f64) {
+    fn move_direction(&mut self, dx: f64, dy: f64) {
         self.x += dx;
         self.y += dy;
     }
@@ -25,14 +25,18 @@ pub struct Rectangle {
     /// width of the rectangle
     width: f64,
     /// center of the rectangle
-    center: Point
+    center: Point,
 }
 
 impl Rectangle {
     /// create a new rectangle.
     pub fn new(l: f64, w: f64, x: f64, y: f64) -> Self {
         let p = Point::new(x, y);
-        Rectangle {length: l, width: w, center: p}
+        Rectangle {
+            length: l,
+            width: w,
+            center: p,
+        }
     }
 
     /// calculate the area of the rectangle
@@ -44,7 +48,7 @@ impl Rectangle {
         2.0 * self.length + 2.0 * self.width
     }
 
-    pub fn move_direction(&mut self, dx: f64, dy:f64) {
+    pub fn move_direction(&mut self, dx: f64, dy: f64) {
         self.center.move_direction(dx, dy);
     }
 }
@@ -60,14 +64,14 @@ mod tests {
     }
 
     #[test]
-   fn circumference() {
+    fn circumference() {
         let my_rect = Rectangle::new(2.0, 3.0, 0.0, 0.0);
         assert_eq!(my_rect.circumference(), 10.0)
     }
 
     #[test]
     fn move_point() {
-        let mut my_point = Point::new(1.0,1.0);
+        let mut my_point = Point::new(1.0, 1.0);
         my_point.move_direction(-1.0, -1.0);
         assert_eq!(my_point.x, 0.0);
         assert_eq!(my_point.y, 0.0);
@@ -75,7 +79,7 @@ mod tests {
 
     #[test]
     fn move_rectangle() {
-        let mut my_rect = Rectangle::new(1.0,1.0, 1.0, 1.0);
+        let mut my_rect = Rectangle::new(1.0, 1.0, 1.0, 1.0);
         my_rect.move_direction(-1.0, -1.0);
         assert_eq!(my_rect.center.x, 0.0);
         assert_eq!(my_rect.center.y, 0.0);
