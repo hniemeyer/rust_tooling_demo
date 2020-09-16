cbf86e3b14120f20a2b58f08879e2ab561107104
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2020-09-16 14:27:56 +0200

    Use serde_json to serialize to json

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ mod geometry;
 fn main() {
     let rect = geometry::Rectangle::new(3.2, 4.5);
     println!("{}", rect.area());
+    let serialized = serde_json::to_string(&rect).unwrap();
+    println!("serialized = {}", serialized);
+
 }
