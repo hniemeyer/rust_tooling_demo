cc7f41054a59d73658366452f07cd88c3205ce78
Hendrik Niemeyer <hendrik.niemeyer@gmail.com>
2020-09-16 14:24:11 +0200

    Removed ndarray

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,6 @@
-use ndarray::prelude::*;
 mod geometry;
 
-/// Changes the 0,0 element of a 2d array of floats.
-/// # Examples
-///
-/// ```
-/// let mut a = array![
-///    [1.,2.,3.], 
-///    [4.,5.,6.],
-/// ]; 
-/// change_elem(&mut a);
-///
-/// assert_eq!(2.0, a[[0,0]]);
-/// ```
-fn change_elem(x: &mut Array2<f64>) {
-
-    x[[0,0]] += 1.0;
-
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn addition() {
-        let mut a = array![
-                [1.,2.,3.], 
-                [4.,5.,6.],
-            ]; 
-            change_elem(&mut a);
-
-        assert_eq!(a[[0,0]],2.0);
-    }
-}
-
 fn main() {
-    let mut a = array![
-                [1.,2.,3.], 
-                [4.,5.,6.],
-            ]; 
     let rect = geometry::Rectangle::new(3.2, 4.5);
-    change_elem(&mut a);
-    println!("{:?}", a);
     println!("{}", rect.area());
 }
