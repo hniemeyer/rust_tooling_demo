c55905ab9b8fad1c4ebc33889cfdf29076902159
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2020-09-30 13:58:33 +0200

    move rectangle in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod geometry;
 
 fn main() {
-    let rect = geometry::Rectangle::new(3.2, 4.5, 21.2, 33.2);
+    let mut rect = geometry::Rectangle::new(3.2, 4.5, 21.2, 33.2);
+    rect.move_direction(1.2, 3.2);
     println!("{}", rect.area());
     println!("{}", rect.circumference());
     let serialized = serde_json::to_string(&rect).unwrap();
